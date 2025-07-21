1a47c6a6168b007d028e74a4b81e21b45ff222f3
Timo Köthe <timo.info@t-online.de>
2025-07-21 16:29:47 +0200

    Initial project setup for Localframe app
    Add Xcode project files, asset catalogs, and initial SwiftUI source files for the Localframe iOS application. Includes basic ContentView and App entry point.

---
+++ Localframe/LocalframeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalframeApp.swift
+//  Localframe
+//
+//  Created by Timo Köthe on 19.07.25.
+//
+
+import SwiftUI
+
+@main
+struct LocalframeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
